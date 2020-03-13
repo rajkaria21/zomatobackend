65f4b74b1bd7059319964a066deee3841c035f38
rajkaria98 <raj.karia.sa@gmail.com>
2020-03-13 18:47:24 +0530

    add review done

---
+++ controller/restaurent/addreview.controller.js
@@ -0,0 +1,72 @@
+const multer = require('multer');
+const DIR = '/home/rajkaria/Documents/Zomato/controller/restaurent/uploads/image';
+const path = require('path');
+const con = require('../../config/connection')
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, DIR);
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.fieldname + Date.now() + path.extname(file.originalname));
+    }
+});
+
+let fileFilter = function (req, file, cb) {
+    var allowedMimes = ['image/jpg', 'image/jpeg', 'image/png'];
+    if (allowedMimes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb({
+            success: false,
+            message: 'Invalid file type. Only jpg, png image files are allowed.'
+        }, false);
+    }
+};
+let obj = {
+    storage: storage,
+    limits: {
+        fileSize: 200 * 1024 * 1024
+    },
+    fileFilter: fileFilter
+};
+
+
+const upload = multer(obj).single('photo'); // upload.single('file')
+module.exports.addreview = (req, res) => {
+    upload(req, res, function (error) {
+        if (error) { //instanceof multer.MulterError
+            res.status(500);
+            if (error.code == 'LIMIT_FILE_SIZE') {
+                error.message = 'File Size is too large. Allowed file size is 200KB';
+                error.success = false;
+            }
+            return res.json(error);
+        } else {
+            if (!req.file) {
+                res.status(500);
+                res.json('file not found');
+            } else {
+
+                const r_id = req.body.r_id;
+                const email = req.body.email;
+                const comment = req.body.comment;
+                const photo = req.file.path;
+                const token = req.headers['auth_token'];
+
+                con.query(`select auth_token from user where auth_token='${token}'`, (err, result) => {
+                    if (result.length != 0) {
+                        con.query(`INSERT INTO review(r_id,email,comment,photo) VALUES ('${r_id}','${email}','${comment}','${photo}')`, function (err, result) {
+                            if (err) {
+                                res.json({ 'error': true, 'message': 'Error Adding Review' });
+                            } else {
+                                res.json({ 'success': true, 'message': 'Review Added Successfully' });
+                            }
+                        });
+                    } else {
+                        res.json({ 'error': true, 'message': 'Wrong Auth Token' });
+                    }
+                });
+            }
+        }
+    })
+};
--- route/restaurent.route.js
+++ route/restaurent.route.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const app = express.Router();
-const path = require('path');
+// const path = require('path');
 const con = require('../config/connection')
-const multer = require('multer');
+// const multer = require('multer');
 // const multipart = require('multiparty')
-const DIR = '/home/rajkaria/Documents/Zomato/controller/restaurent/uploads/image';
-const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-        callback(null, DIR);
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + Date.now() + path.extname(file.originalname));
-    }
-});
+// const DIR = '/home/rajkaria/Documents/Zomato/controller/restaurent/uploads/image';
+// const storage = multer.diskStorage({
+//     destination: function (req, file, callback) {
+//         callback(null, DIR);
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.fieldname + Date.now() + path.extname(file.originalname));
+//     }
+// });
 
 
-const fileFilter = (req,file,cb) => {
+// const fileFilter = function (req, file, cb) {
 
-    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true);
-    } else {
-        return cb(new Error('Only images allowed'));   
-    }
-};
+//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//         cb(null, true);
+//     } else {
+//         cb({
+//             success: false,
+//             message: 'Invalid file type. Only jpg, png image files are allowed.'
+//         }, false);
+//     }
+// };
 
-const upload = multer({
-    storage:storage,fileFilter:fileFilter
-});
+// const upload = multer({
+//     storage: storage, fileFilter: fileFilter
+// });
 
 const getrestaurentcontrol = require('../controller/restaurent/getrestaurent.controller');
 const getrestaurentdetailcontrol = require('../controller/restaurent/getrestaurentdetail.controller');
@@ -35,37 +38,37 @@ const addratingcontrol = require('../controller/restaurent/addrating.controller'
 const viewaverageratingcontrol = require('../controller/restaurent/viewaveragerating.controller');
 const getreviewcontrol = require('../controller/restaurent/getreviews.controller');
 // const reviewvalidate = require('../middleware/reviewvalidation');
-
+const addreviewcontrol = require('../controller/restaurent/addreview.controller');
 app.get('/res/getreviews', getreviewcontrol.getreview);
 app.get('/res/restaurents', getrestaurentcontrol.getrestaurents);
 app.get('/res/restaurentdetail', getrestaurentdetailcontrol.getrestaurentdetails);
 app.get('/res/search', searchrestaurentcontrol.searchbyrestaurent);
 app.post('/res/addrating', addratingcontrol.addrating);
 app.get('/res/viewrating', viewaverageratingcontrol.viewaveragerating);
-app.post('/res/addreview', upload.single('photo'),function (req, res) {
- 
-    const r_id = req.body.r_id;
-    const email = req.body.email;
-    const comment = req.body.comment;
-    const photo = req.file.path;
-    const token = req.headers['auth_token'];
+app.post('/res/addreview',addreviewcontrol.addreview)
 
-        con.query(`select auth_token from user where auth_token='${token}'`, (err, result) => {
-            if (result.length != 0) {
-                
-                con.query(`INSERT INTO review(r_id,email,comment,photo) VALUES ('${r_id}','${email}','${comment}','${photo}')`, function (err, result) {
-                    if (err) {
-                     
-                        res.json({ 'error': true, 'message': 'Error Adding Review' });
-                    } else {
-                        res.json({ 'success': true, 'message': 'Review Added Successfully' });
-                    }
-                });
-            } else {
-                res.json({ 'error': true, 'message': 'Wrong Auth Token' });
-            }
-        });
-});
+    // const r_id = req.body.r_id;
+    // const email = req.body.email;
+    // const comment = req.body.comment;
+    // const photo = req.file.path;
+    // const token = req.headers['auth_token'];
+
+    // con.query(`select auth_token from user where auth_token='${token}'`, (err, result) => {
+    //     if (result.length != 0) {
+
+    //         con.query(`INSERT INTO review(r_id,email,comment,photo) VALUES ('${r_id}','${email}','${comment}','${photo}')`, function (err, result) {
+    //             if (err) {
+
+    //                 res.json({ 'error': true, 'message': 'Error Adding Review' });
+    //             } else {
+    //                 res.json({ 'success': true, 'message': 'Review Added Successfully' });
+    //             }
+    //         });
+    //     } else {
+    //         res.json({ 'error': true, 'message': 'Wrong Auth Token' });
+    //     }
+    // });
+
 
 
 module.exports = app;
